689600a0304b93c1481e1609624a229e3d962cf3
timothy-lew <timothyjblew2000@gmail.com>
2025-01-06 17:07:20 +0800

    rename cols to PascalCase for prescription_list

--- app/models/patient_prescription_list_model.py
+++ app/models/patient_prescription_list_model.py
@@ -6,11 +6,11 @@ from app.database import Base
 class PatientPrescriptionList(Base):
     __tablename__ = "PATIENT_PRESCRIPTION_LIST"
 
-    id = Column(Integer, primary_key=True, index=True)  # Changed to Integer
-    active = Column(String(1), default='0', nullable=False)  # used to check if record is active or not, substitute isDeleted column
-    createdDateTime = Column(DateTime, nullable=False, default=datetime.now)
-    modifiedDateTime = Column(DateTime, nullable=False, default=datetime.now)
-    value = Column(String(255))
+    Id = Column(Integer, primary_key=True, index=True)  # Changed to Integer
+    IsDeleted = Column(String(1), default='0', nullable=False)  # used to check if record is active or not, substitute isDeleted column
+    CreatedDateTime = Column(DateTime, nullable=False, default=datetime.now)
+    UpdatedDateTime = Column(DateTime, nullable=False, default=datetime.now)
+    Value = Column(String(255))
 
     prescriptions = relationship(
         "PatientPrescription",
