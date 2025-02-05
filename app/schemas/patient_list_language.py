6e68050ec8f16a1fc320d090ed91b6bae5fce733
weiyuan12 <weiyuan.tan0@gmail.com>
2025-02-06 00:06:22 +0800

    Fix model and schema

--- app/schemas/patient_list_language.py
+++ app/schemas/patient_list_language.py
@@ -1,14 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import Optional
 
-class ListLanguageBase(BaseModel):
-    isDeleted: str                                      
-    createdDateTime: datetime                               
-    modifiedDateTime: datetime  
+class PatientListLanguageBase(BaseModel):
+    isDeleted: Optional[str] = Field(default="0", json_schema_extra={"example": "0"})                                      
     value: str                            
 
-class ListLanguage(ListLanguageBase):
-    id: int # INT -> int (primary key)
+class PatientListLanguageCreate(PatientListLanguageBase):
+    pass
 
+class PatientListLanguageUpdate(PatientListLanguageBase):
+    pass
+class PatientListLanguage(PatientListLanguageBase):
+    id: int
+    createdDate: datetime
+    modifiedDate: datetime
     model_config = {"from_attributes": True}
