1beedbc7f81a47fb290bf134d57f315d8c1229f4
weiyuan12 <weiyuan.tan0@gmail.com>
2025-02-08 23:42:16 +0800

    refactor patient router to follow conventions

--- app/routers/patient_router.py
+++ app/routers/patient_router.py
@@ -20,17 +20,17 @@ def read_patient(patient_id: int, db: Session = Depends(get_db), mask: bool = Tr
 
 @router.get("/patients/", response_model=PaginatedResponse[Patient])
 def read_patients(mask: bool = True, pageNo: int = 0, pageSize: int = 10, db: Session = Depends(get_db)):
-    patients, totalRecords, totalPages = crud_patient.get_patients(db=db, pageNo=pageNo, pageSize=pageSize, mask=mask)
-    patients = [Patient.model_validate(patient) for patient in patients]
+    db_patients, totalRecords, totalPages = crud_patient.get_patients(db=db, pageNo=pageNo, pageSize=pageSize, mask=mask)
+    patients = [Patient.model_validate(patient) for patient in db_patients]
     return PaginatedResponse(data=patients, pageNo=pageNo, pageSize=pageSize, totalRecords= totalRecords, totalPages=totalPages)
 
-@router.post("/patients/", response_model=SingleResponse[Patient])
+@router.post("/patients/add", response_model=SingleResponse[Patient])
 def create_patient(patient: PatientCreate, db: Session = Depends(get_db)):
     db_patient = crud_patient.create_patient(db=db, patient=patient)
     patient = Patient.model_validate(db_patient)
     return SingleResponse(data=patient)
 
-@router.put("/patients/{patient_id}", response_model=SingleResponse[Patient])
+@router.put("/patients/update/{patient_id}", response_model=SingleResponse[Patient])
 def update_patient(patient_id: int, patient: PatientUpdate, db: Session = Depends(get_db)):
     db_patient = crud_patient.update_patient(db=db, patient_id=patient_id, patient=patient)
     if db_patient is None:
@@ -38,7 +38,7 @@ def update_patient(patient_id: int, patient: PatientUpdate, db: Session = Depend
     patient = Patient.model_validate(db_patient)
     return SingleResponse(data=patient)
 
-@router.delete("/patients/{patient_id}", response_model=SingleResponse[Patient])
+@router.delete("/patients/delete/{patient_id}", response_model=SingleResponse[Patient])
 def delete_patient(patient_id: int, db: Session = Depends(get_db)):
     db_patient = crud_patient.delete_patient(db=db, patient_id=patient_id)
     if db_patient is None:
