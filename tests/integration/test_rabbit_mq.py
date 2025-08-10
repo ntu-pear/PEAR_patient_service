350014d00b3982985d5ad44c41c27330acf0cd08
Cebelle1 <cebelleloo@hotmail.com>
2025-08-11 01:18:40 +0800

    see if its integ test hanging pyutestttttttttttttttttttttasdwasdwsad

--- tests/integration/test_rabbit_mq.py
+++ tests/integration/test_rabbit_mq.py
@@ -1,47 +1,16 @@
-import pytest
-from app.messaging.patient_publisher import PatientPublisher
-from app.messaging.producer_manager import stop_producer_manager
-
-@pytest.fixture(scope="module")
-def publisher():
-    publisher_instance = PatientPublisher()
-    yield publisher_instance
-    # Cleanup after all tests in module finish
-    publisher_instance.close()
-    stop_producer_manager()  # stop the producer manager and join the thread
-
-def test_publish_patient_created(publisher):
-    patient_id = 456
-    patient_data = {
-        'id': patient_id,
-        'name': 'Jane Smith',
-        'nric': 'S7654321B',
-        'isActive': '1',
-        'startDate': '2024-01-01T00:00:00',
-        'preferredName': 'Janey'
-    }
-    created_by = "patient_integration_test"
-    result = publisher.publish_patient_created(patient_id, patient_data, created_by)
-    assert result is True
-
-#def test_publish_patient_updated(publisher):
-#    patient_id = 456
-#    old_data = {
-#        'id': patient_id,
-#        'name': 'Jane Smith',
-#        'nric': 'S7654321B',
-#        'isActive': '1',
-#        'startDate': '2024-01-01T00:00:00',
-#        'preferredName': 'Janey'
-#    }
-#    new_data = old_data.copy()
-#    new_data['preferredName'] = 'Jane'
-#    changes = {'preferredName': {'old': 'Janey', 'new': 'Jane'}}
-#    modified_by = "patient_integration_test"
-#    result = publisher.publish_patient_updated(patient_id, old_data, new_data, changes, modified_by)
-#    assert result is True
+#import pytest
+#from app.messaging.patient_publisher import PatientPublisher
+#from app.messaging.producer_manager import stop_producer_manager
 #
-#def test_publish_patient_deleted(publisher):
+#@pytest.fixture(scope="module")
+#def publisher():
+#    publisher_instance = PatientPublisher()
+#    yield publisher_instance
+#    # Cleanup after all tests in module finish
+#    publisher_instance.close()
+#    stop_producer_manager()  # stop the producer manager and join the thread
+#
+#def test_publish_patient_created(publisher):
 #    patient_id = 456
 #    patient_data = {
 #        'id': patient_id,
@@ -49,8 +18,39 @@ def test_publish_patient_created(publisher):
 #        'nric': 'S7654321B',
 #        'isActive': '1',
 #        'startDate': '2024-01-01T00:00:00',
-#        'preferredName': 'Jane'
+#        'preferredName': 'Janey'
 #    }
-#    deleted_by = "patient_integration_test"
-#    result = publisher.publish_patient_deleted(patient_id, patient_data, deleted_by)
+#    created_by = "patient_integration_test"
+#    result = publisher.publish_patient_created(patient_id, patient_data, created_by)
 #    assert result is True
+#
+##def test_publish_patient_updated(publisher):
+##    patient_id = 456
+##    old_data = {
+##        'id': patient_id,
+##        'name': 'Jane Smith',
+##        'nric': 'S7654321B',
+##        'isActive': '1',
+##        'startDate': '2024-01-01T00:00:00',
+##        'preferredName': 'Janey'
+##    }
+##    new_data = old_data.copy()
+##    new_data['preferredName'] = 'Jane'
+##    changes = {'preferredName': {'old': 'Janey', 'new': 'Jane'}}
+##    modified_by = "patient_integration_test"
+##    result = publisher.publish_patient_updated(patient_id, old_data, new_data, changes, modified_by)
+##    assert result is True
+##
+##def test_publish_patient_deleted(publisher):
+##    patient_id = 456
+##    patient_data = {
+##        'id': patient_id,
+##        'name': 'Jane Smith',
+##        'nric': 'S7654321B',
+##        'isActive': '1',
+##        'startDate': '2024-01-01T00:00:00',
+##        'preferredName': 'Jane'
+##    }
+##    deleted_by = "patient_integration_test"
+##    result = publisher.publish_patient_deleted(patient_id, patient_data, deleted_by)
+##    assert result is True
