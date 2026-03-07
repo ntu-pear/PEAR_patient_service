ae750c1535f3afa141521cc050854735e7c2200b
nichtyq <nicholastanyq@gmail.com>
2026-03-07 14:55:24 +0800

    fix id autoincrement=false for integration testing

--- app/models/ref_userconfig_model.py
+++ app/models/ref_userconfig_model.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, Integer, String, DateTime, Numeric, ForeignKey
 class RefUserConfig(Base):
     __tablename__ = "REF_USERCONFIG"
 
-    UserConfigID = Column(Integer, primary_key=True, index=True)
+    UserConfigID = Column(Integer, primary_key=True, autoincrement=False, index=True)
     configBlob = Column(String, nullable=False)
     modifiedDate = Column(DateTime, nullable=False)
     modifiedById = Column(String, nullable=False)
